592b1d506a352c4099d5b6edfb7dad5cee66bb77
JakubNowak884 <56163753+JakubNowak884@users.noreply.github.com>
2021-08-22 20:55:33 +0200

    update

--- Workout_Mobile_App/Workout_Mobile_App/Views/WorkoutEntryPage.xaml.cs
+++ Workout_Mobile_App/Workout_Mobile_App/Views/WorkoutEntryPage.xaml.cs
@@ -38,13 +38,77 @@ namespace Workout_Mobile_App.Views
             List<Day> listOfDays = await App.DatabaseDay.GetDaysAsync(workout.ID);
             List<Exercise> listOfExercises = await App.DatabaseExercise.GetExercisesAsync(listOfDays[workout.Day].ID);
             CurrentExercise = listOfExercises[workout.Exercise];
-            contentPage.Title = CurrentExercise.Name.ToString() + " - Week " + workout.Week.ToString();
-
-            MainViewModel test = new MainViewModel()
+            textTitle = CurrentExercise.Name.ToString() + " - Week " + (workout.Week + 1).ToString();
+            float percentage = 0;
+            float addToPercentage = 0;
+            switch (CurrentExercise.Type)
             {
-                Text = text
-            };
-            BindingContext = test;
+                case TypeOfProgress.Standard:
+                    text = "Today's weight: " + CurrentExercise.Weight.ToString();
+                    break;
+
+                case TypeOfProgress.Jim_Wendler:
+                    switch (workout.Week % 4)
+                    {
+                        case 0:
+                            percentage = 0.75f;
+                            addToPercentage = 0.05f;
+                            break;
+                        case 1:
+                            percentage = 0.80f;
+                            addToPercentage = 0.05f;
+                            break;
+                        case 2:
+                            percentage = 0.75f;
+                            addToPercentage = 0.10f;
+                            break;
+                        case 3:
+                            percentage = 0.40f;
+                            addToPercentage = 0.10f;
+                            break;
+                        default:
+                            break;
+                    }
+                    text = "Today's weight for first set: " + (CurrentExercise.Weight * percentage).ToString() + '\n';
+                    percentage += addToPercentage;
+                    text += "Today's weight for second set: " + (CurrentExercise.Weight * percentage).ToString() + '\n';
+                    percentage += addToPercentage;
+                    text += "Today's weight for third set: " + (CurrentExercise.Weight * percentage).ToString() + '\n';
+                    break;
+
+                case TypeOfProgress.Jim_Wendler_Plus_Sidework:
+                    switch (workout.Week % 4)
+                    {
+                        case 0:
+                            percentage = 0.75f;
+                            addToPercentage = 0.05f;
+                            break;
+                        case 1:
+                            percentage = 0.80f;
+                            addToPercentage = 0.05f;
+                            break;
+                        case 2:
+                            percentage = 0.75f;
+                            addToPercentage = 0.10f;
+                            break;
+                        case 3:
+                            percentage = 0.40f;
+                            addToPercentage = 0.10f;
+                            break;
+                    }
+                    text = "Today's weight for first set: " + (CurrentExercise.Weight * percentage).ToString() + '\n';
+                    percentage += addToPercentage;
+                    text += "Today's weight for second set: " + (CurrentExercise.Weight * percentage).ToString() + '\n';
+                    percentage += addToPercentage;
+                    text += "Today's weight for third set: " + (CurrentExercise.Weight * percentage).ToString() + '\n';
+                    text += "Today's weight for 5x10: " + (CurrentExercise.Weight * 0.6).ToString() + '\n';
+                    break;
+
+                default:
+                    break;
+            }
+            BindingContext = text;
+            contentPage.Title = textTitle;
         }
 
         void LoadWorkout(string itemId)
@@ -158,42 +222,140 @@ namespace Workout_Mobile_App.Views
                 default:
                     break;
             }
-            MainViewModel test = (MainViewModel)BindingContext;
-            test.Text = text;
-            BindingContext = test;
-            workout.Title = textTitle;
+            BindingContext = text;
+            contentPage.Title = textTitle;
             await App.DatabaseWorkout.UpdateWorkoutAsync(workout);
-            contentPage.Title = workout.Title;
         }
         async void OnContinueWithDefaultProgressClicked(object sender, EventArgs e)
         {
-            CurrentExercise.Weight += CurrentExercise.Progress;
-            await App.DatabaseExercise.UpdateExerciseAsync(CurrentExercise);
+            Workout workout = await App.DatabaseWorkout.GetWorkoutAsync(CurrentWorkoutID);
+            switch (CurrentExercise.Type)
+            {
+                case TypeOfProgress.Standard:
+                    CurrentExercise.Weight += CurrentExercise.Progress;
+                    await App.DatabaseExercise.UpdateExerciseAsync(CurrentExercise);
 
-            SetBindingContext();
+                    SetBindingContext();
+                    break;
+                case TypeOfProgress.Jim_Wendler:
+                    if (workout.Week % 4 == 0)
+                    {
+                        CurrentExercise.Weight += CurrentExercise.Progress;
+                        await App.DatabaseExercise.UpdateExerciseAsync(CurrentExercise);
+
+                        SetBindingContext();
+                    }
+                    else
+                    {
+                        await DisplayAlert("Warning", "In 5/3/1 you can progress only on week four", "Ok");
+                    }
+                    break;
+                case TypeOfProgress.Jim_Wendler_Plus_Sidework:
+                    if (workout.Week % 4 == 0)
+                    {
+                        CurrentExercise.Weight += CurrentExercise.Progress;
+                        await App.DatabaseExercise.UpdateExerciseAsync(CurrentExercise);
+
+                        SetBindingContext();
+                    }
+                    else
+                    {
+                        await DisplayAlert("Warning", "In 5/3/1 you can progress only on week four", "Ok");
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
         async void OnContinueWithAlternativeProgressClicked(object sender, EventArgs e)
         {
-            string result = await DisplayPromptAsync("Progress (in kg)", "Type alternative progress:");
-            try
+            Workout workout = await App.DatabaseWorkout.GetWorkoutAsync(CurrentWorkoutID);
+            string result = string.Empty;
+            switch (CurrentExercise.Type)
             {
-                CurrentExercise.Progress = float.Parse(result);
-            }
-            catch (ArgumentNullException)
-            {
-                Console.WriteLine("Typed value is empty.");
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Typed value is not a number.");
-            }
-            catch (OverflowException)
-            {
-                Console.WriteLine("Typed value is out of range.");
-            }
-            await App.DatabaseExercise.UpdateExerciseAsync(CurrentExercise);
+                case TypeOfProgress.Standard:
+                    result = await DisplayPromptAsync("Progress (in kg)", "Type alternative progress:");
+                    try
+                    {
+                        CurrentExercise.Weight += float.Parse(result);
+                    }
+                    catch (ArgumentNullException)
+                    {
+                        Console.WriteLine("Typed value is empty.");
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Typed value is not a number.");
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine("Typed value is out of range.");
+                    }
+                    await App.DatabaseExercise.UpdateExerciseAsync(CurrentExercise);
 
-            SetBindingContext();
+                    SetBindingContext();
+                    break;
+                case TypeOfProgress.Jim_Wendler:
+                    if (workout.Week % 4 == 0)
+                    {
+                        result = await DisplayPromptAsync("Progress (in kg)", "Type alternative progress:");
+                        try
+                        {
+                            CurrentExercise.Weight += float.Parse(result);
+                        }
+                        catch (ArgumentNullException)
+                        {
+                            Console.WriteLine("Typed value is empty.");
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("Typed value is not a number.");
+                        }
+                        catch (OverflowException)
+                        {
+                            Console.WriteLine("Typed value is out of range.");
+                        }
+                        await App.DatabaseExercise.UpdateExerciseAsync(CurrentExercise);
+
+                        SetBindingContext();
+                    }
+                    else
+                    {
+                        await DisplayAlert("Warning", "In 5/3/1 you can progress only on week four", "Ok");
+                    }
+                    break;
+                case TypeOfProgress.Jim_Wendler_Plus_Sidework:
+                    if (workout.Week % 4 == 0)
+                    {
+                        result = await DisplayPromptAsync("Progress (in kg)", "Type alternative progress:");
+                        try
+                        {
+                            CurrentExercise.Weight += float.Parse(result);
+                        }
+                        catch (ArgumentNullException)
+                        {
+                            Console.WriteLine("Typed value is empty.");
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("Typed value is not a number.");
+                        }
+                        catch (OverflowException)
+                        {
+                            Console.WriteLine("Typed value is out of range.");
+                        }
+                        await App.DatabaseExercise.UpdateExerciseAsync(CurrentExercise);
+
+                        SetBindingContext();
+                    }
+                    else
+                    {
+                        await DisplayAlert("Warning", "In 5/3/1 you can progress only on week four", "Ok");
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
         async void OnContinueWithoutProgressClicked(object sender, EventArgs e)
         {
@@ -203,10 +365,44 @@ namespace Workout_Mobile_App.Views
         }
         async void OnDeloadClicked(object sender, EventArgs e)
         {
-            CurrentExercise.Weight *= CurrentExercise.Deload * 0.01f;
-            await App.DatabaseExercise.UpdateExerciseAsync(CurrentExercise);
+            Workout workout = await App.DatabaseWorkout.GetWorkoutAsync(CurrentWorkoutID);
+            switch (CurrentExercise.Type)
+            {
+                case TypeOfProgress.Standard:
+                    CurrentExercise.Weight *= CurrentExercise.Deload * 0.01f;
+                    await App.DatabaseExercise.UpdateExerciseAsync(CurrentExercise);
 
-            SetBindingContext();
+                    SetBindingContext();
+                    break;
+                case TypeOfProgress.Jim_Wendler:
+                    if (workout.Week % 4 == 0)
+                    {
+                        CurrentExercise.Weight *= CurrentExercise.Deload * 0.01f;
+                        await App.DatabaseExercise.UpdateExerciseAsync(CurrentExercise);
+
+                        SetBindingContext();
+                    }
+                    else
+                    {
+                        await DisplayAlert("Warning", "In 5/3/1 you can deload only on week four", "Ok");
+                    }
+                    break;
+                case TypeOfProgress.Jim_Wendler_Plus_Sidework:
+                    if (workout.Week % 4 == 0)
+                    {
+                        CurrentExercise.Weight *= CurrentExercise.Deload * 0.01f;
+                        await App.DatabaseExercise.UpdateExerciseAsync(CurrentExercise);
+
+                        SetBindingContext();
+                    }
+                    else
+                    {
+                        await DisplayAlert("Warning", "In 5/3/1 you can deload only on week four", "Ok");
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
